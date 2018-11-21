60511e5504e689a54c0a8247758bdd891efebb60
wskyhrb <41772742+wskyhrb@users.noreply.github.com>
2018-11-22 01:14:04 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "!"// البرفكس هنا
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('By ')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+
+if (message.content.startsWith('!!')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/MeeRcY");
+  client.user.setStatus("idle")
+   message.channel.send(`${argresult} \` `)
+}
+});  
+  
+
+
+});///الكود فوق ذي
+ 
+ 
+ 
+client.login(process.env.BOT_TOKEN);
